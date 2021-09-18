1672640ab97a7c4799eb02ce9af7174fa79ac4ac
Laurent NARSIMOULOU <nrslaurent@icloud.com>
2021-09-18 15:23:12 +0200

    add webpack bundle and bootstrap

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,11 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
+// jquery
+const $ = require('jquery');
+require('bootstrap');
+
 // start the Stimulus application
 import './bootstrap';
+
+
